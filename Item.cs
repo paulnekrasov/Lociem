26dd0331107eb7fc9d35f20c8cd563a93cc172bf
Olha Nekrasova <Olha Nekrasova@DESKTOP-K3OJFER>
2026-03-02 09:20:04 +0200

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Item
+{
+	public class Item
+	{
+		public CreateItem()
+		{
+			private int id;
+			private string name;
+			private string description;
+		public Item(int id, string name, string description) {
+			this.id = id; 
+			this.name = name;
+			this.description = description;
+		}
+			
+		}
+	}
+}
---
+++ Storagelocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Storagelocation
+{
+    public class StorageLocation
+    {
+        CreateLocation shelf = new CreateLocation("Shelf", "A place for storing pickles"); 
+
+
+        public CreateLocation()
+        {
+           private int id;
+           private string name;
+           private string description;
+
+        public StorageLocation(int id, string name, string description) {
+
+            this.id = id; 
+            this.name = name;
+            this.description = description;
+
+        }
+
+
+            
+        }
+    }
+}
+
