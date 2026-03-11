e1f7db78ec72dbc9fdf6a494b2b9948e10e7ed64
Paul Nekrasov <paulneroxon@gmail.com>
2026-03-11 16:53:45 +0200

    refactor: completed practice 3

---
+++ Lociem/Managers/ItemManager.cs
@@ -0,0 +1,53 @@
+﻿using Lociem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lociem.Interfaces;
+
+namespace Lociem.Services
+{
+    public class ItemManager : IRepository<Item>
+    {
+        List<Item> items = new List<Item>();
+        
+        public void Add(Item item)
+        {
+            items.Add(item);
+        }
+
+       public void Delete(Item item)
+        {
+
+            Item? itemToDelete = items.Find(i => i.Id == item.Id);
+
+            if (itemToDelete != null)
+            {
+                items.Remove(itemToDelete);
+            }
+        }
+        public void Update(Item item)
+        {
+
+            Item? itemcheck = items.Find(i => i.Id == item.Id);      
+            itemcheck?.Rename(item.Name);
+            itemcheck?.ChangeDescription(item.Description);
+            itemcheck?.AssignToStorageLocation(item.StorageLocation);
+        }
+
+        public Item? FindbyName(string name)
+        {
+            return items.Find(i => i.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public Item? FindbyId(int id)
+        {
+            return items.Find(i => i.Id == id);
+        }
+        public List<Item> GetAll()
+        {
+            return new List<Item>(items);
+        }
+
+
+    }
+}
---
+++ Lociem/Managers/StorageLocationManager.cs
@@ -0,0 +1,51 @@
+﻿using Lociem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lociem.Interfaces;
+
+namespace Lociem.Services
+{
+    public class StorageLocationManager : IRepository<StorageLocation>
+    {
+        List<StorageLocation> storageLocations = new List<StorageLocation>();
+
+        public void Add(StorageLocation storageLocation)
+        {
+            storageLocations.Add(storageLocation);
+        }
+
+        public void Delete(StorageLocation storageLocation)
+        {
+
+            StorageLocation? locationToDelete = storageLocations.Find(s => s.Id == storageLocation.Id);
+
+            if (locationToDelete != null)
+            {
+                storageLocations.Remove(locationToDelete);
+            }
+
+        }
+
+        public void Update(StorageLocation storageLocation)
+        {
+            StorageLocation? locationCheck = storageLocations.Find(s => s.Id == storageLocation.Id);
+            locationCheck?.Rename(storageLocation.Name);
+            locationCheck?.ChangeDescription(storageLocation.Description);
+        }
+
+        public StorageLocation? FindbyName(string name)
+        {
+            return storageLocations.Find(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public StorageLocation? FindbyId(int id)
+        {
+            return storageLocations.Find(s => s.Id == id);
+        }
+        public List<StorageLocation> GetAll() {
+
+            return new List<StorageLocation>(storageLocations);
+        }
+    }
+}
